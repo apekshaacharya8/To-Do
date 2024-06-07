5adf899f1c17eee658e7a4487d4aeceb946f9877
Apeksha <apeksha@zycus.com>
2024-06-07 19:22:31 +0530

    todo app

--- src/app/Model/todo.interface.ts
+++ src/app/Model/todo.interface.ts
@@ -3,3 +3,8 @@ export interface Todo {
   completed : boolean;
   description : string;
 }
+
+export interface Item{
+  label : string;
+  image : string ;
+}
--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from 'src/app/Model/todo.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashBoardComponent {
-
+  items : Item[]=[
+    {
+      label : "Add Task" ,
+      image: ""
+    },
+    {
+      label : "Add Task" ,
+      image: ""
+    }
+  ];
 }
